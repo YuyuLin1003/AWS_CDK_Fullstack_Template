22a3faeffbd7a85fd18b6858394bdd7b1e90fc68
yuyulin <yuyulin256@gmail.com>
2025-08-27 17:06:11 +0200

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      message: "Hello, World!",
+      input: event,
+    }),
+  };
+};
